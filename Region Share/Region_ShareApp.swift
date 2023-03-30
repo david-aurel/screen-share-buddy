32537acbc6ddd43130e2b48583fd51474861ff55
David Durlan <53895961+david-aurel@users.noreply.github.com>
2023-03-30 20:51:04 +0200

    Initial Commit

---
+++ Region Share/Region_ShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Region_ShareApp.swift
+//  Region Share
+//
+//  Created by David on 30.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Region_ShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
