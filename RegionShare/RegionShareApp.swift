8c7cdf4b31ed846c0a540eb1d1a23ec8625f98ce
David Durlan <53895961+david-aurel@users.noreply.github.com>
2023-04-30 13:46:37 +0200

    feat: transparent title bar

--- RegionShare/RegionShareApp.swift
+++ RegionShare/RegionShareApp.swift
@@ -7,8 +7,18 @@
 
 import SwiftUI
 
+// override style of the main window as soon as it has been created.
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        if let window = NSApplication.shared.windows.first {
+            window.titlebarAppearsTransparent = true
+        }
+    }
+}
+
 @main
 struct RegionShareApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
